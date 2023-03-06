471caab52b3e90d73307837733a1b1e1cfd9281c
Mads Kristensen <madskristensen@users.noreply.github.com>
2023-03-06 15:22:23 -0800

    Adding ARM support [release]

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -9,7 +9,7 @@ namespace BionicReading
     {
         public const string Id = "BionicReading.5fc630d9-8214-4730-b3e7-60d50c1eaa1b";
         public const string Name = "Bionic Reading";
-        public const string Description = @"Bionic Reading uses bold text to on the first part of words, which makes the brain complete the rest more easily. This makes it easier for some people to read text and code";
+        public const string Description = @"Bionic Reading uses bold text on the first part of words, which makes the brain complete the rest more easily. This makes it easier for some people to read text and code";
         public const string Language = "en-US";
         public const string Version = "1.0";
         public const string Author = "Mads Kristensen";
